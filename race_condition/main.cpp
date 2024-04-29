2f679f473971e39d611cf6d1a3f3578234373b0f
MihaS <mishastatnik@gmail.com>
2024-04-29 19:21:01 +0300

    task 2.3

--- race_condition/main.cpp
+++ race_condition/main.cpp
@@ -3,11 +3,13 @@
 #include <memory>
 #include <iostream>
 #include <atomic>
+#include <unistd.h>
 
+long count = 1E8;
 
 void no_protection(long int& value) 
 {
-    for(int i = 0; i < 1E9; i++)
+    for(int i = 0; i < count; i++)
         value = value + 1;
     
     return;
@@ -16,7 +18,7 @@ void no_protection(long int& value)
 std::mutex the_mutex;
 void with_mutex(long int& value)
 {
-    for (size_t i = 0; i < 1E9; i++)
+    for (size_t i = 0; i < count; i++)
     {
         {
             std::lock_guard guard{the_mutex};
@@ -29,7 +31,7 @@ void with_mutex(long int& value)
 void with_atomic(std::atomic<long>& value)
 {
     long oldvalue, newvalue;
-    for (size_t i = 0; i < 1E9; i++)
+    for (size_t i = 0; i < count; i++)
     {
         do
         {
@@ -40,22 +42,49 @@ void with_atomic(std::atomic<long>& value)
     }
 }
 
+void sync_increase(long int& counter)
+{
+    for(int i = 0; i < 1000; i++) {
+        int current = counter;
+        std::this_thread::yield();
+        counter = current + 1;
+        std::this_thread::yield();
+    }
+}
+
 int main()
 {
     long int value = 0;
     std::atomic<long> atomic_value;
     atomic_value.exchange(0);
+    std::cout << "pid: " << getpid() << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(10));
 
-    auto f = with_mutex;
+    // auto atomic_f = [&atomic_value](long value){
+    //     with_atomic(atomic_value);
+    // };
+    // auto f = no_protection; 
 
-    auto func = [&value, &f]() {
-        f(value);
-    };
-    std::thread other_thread{func};
+    // auto func = [&value, &f]() {
+    //     f(value);
+    // };
+    // std::thread other_thread{func};
 
-    f(value);
-    other_thread.join();
+    // f(value);
+    // other_thread.join();
 
-    std::cout << "Num: " << value << std::endl;
+    // std::cout << "Num: " << value << std::endl;
 
+    value = 0;
+    std::thread sync_thread{[&value](){
+        sync_increase(value);
+    }};
+    std::thread sync_thread2{[&value](){
+        sync_increase(value);
+    }};
+
+    sync_thread2.join();
+    sync_thread.join();
+
+    std::cout << "Value: " << value << std::endl;
 }
